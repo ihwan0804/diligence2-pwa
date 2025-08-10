c0a336885c6f561a6d61501e0ccabb44999bf8f9
ihwan0804 <ihwan0804@gmail.com>
2025-08-10 17:35:23 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "dlu-cache-v9-final-undo-items";
+const CORE_ASSETS = [
+  "./","./index.html","./manifest.json",
+  "./icon-192.png","./icon-512.png",
+  "./click.wav","./success.wav","./fail.wav"
+];
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    ).then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then((c)=>c.put(event.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html")))
+  );
+});
